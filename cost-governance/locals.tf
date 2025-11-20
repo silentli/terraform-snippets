1d02988b492aa9908a3bec33fc40dc5d2a091862
silentli <heylibai@gmail.com>
2025-11-20 16:00:34 +0100

    fix: fix based on tflint

--- cost-governance/locals.tf
+++ cost-governance/locals.tf
@@ -1,8 +1,7 @@
 # Common Tags
-# tflint-ignore: terraform_unused_declarations
 # Note: common_tags is kept for future use when additional cost governance resources are added
 locals {
-  common_tags = merge(
+  common_tags = merge( # tflint-ignore: terraform_unused_declarations
     {
       Project     = var.project_name
       Environment = var.environment
